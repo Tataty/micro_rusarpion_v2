68f627d84b65e69e2fa88396f32e4da22f6794b2
Tataty <ruslanl64kv@gmail.com>
2026-04-06 22:55:08 +0300

    SERVO: add more pwm servos

--- fpga/src/top.sv
+++ fpga/src/top.sv
@@ -5,7 +5,7 @@ module top (
     input  logic uart1_rx_data,
     output logic led_ready,
     output logic led_done,
-    output logic servo_pwm1,
+    output logic [17:0] servo_pwm,
     output logic debug_a10,
     output logic debug_e10,
     output logic debug_l11
@@ -27,51 +27,48 @@ module top (
     );
 
     /* UART FULL DATA */
-    logic [216:0] uart_full_data = 0;
+    logic [215:0] uart_full_data = 0;
     logic uart_full_data_valid = 0;
         
     always_ff @(posedge clk) begin
         if (!reset_n) begin
             uart_full_data <= 0;
+            uart_full_data_valid <= 0;
         end else begin
             uart_full_data_valid <= 0;
 
             if (uart1_data_valid) begin
+                uart_full_data <= {uart1_data, uart_full_data[47:8]};
 
-                case (uart1_data_count)
-                    1: uart_full_data[7:0] <= uart1_data;
-                    2: uart_full_data[15:8] <= uart1_data;
-                    3: uart_full_data[23:16] <= uart1_data;
-                    4: uart_full_data[31:24] <= uart1_data;
-                    5: begin
-                        uart_full_data[15:8] <= uart1_data;
-                        uart_full_data_valid <= 1;
-                    end
-                    default: ;
-                endcase
+                if (uart1_data_count == 18) begin 
+                    uart_full_data_valid <= 1'b1;
+                end
             end
         end
     end
 
     /* SERVO PWM */
-    logic [19:0] pwm_duty = 28000, pwm50hz_reg = 0;
-
     localparam SERVO_DUTY_0 = 28000;  // ~0.5ms
     localparam SERVO_DUTY_180 = 128000; // ~2.5ms
     localparam SERVO_DUTY_90 = (SERVO_DUTY_180 + SERVO_DUTY_0) / 2;
 
+    logic [19:0] pwm_duty [0:17]; 
+
     always_ff @(posedge clk) begin
         if (!reset_n) begin
-            pwm_duty <= 0;
+            pwm_duty <= '{default: SERVO_DUTY_90};
         end else begin
             if (uart_full_data_valid) begin
-                /* 875 -> 4000 */
-                /* 28000 >> 5 = 875 */
-                /* 128000 >> 5 = 4000 */ 
-                pwm_duty <= uart_full_data << 5;
+                /* 875 -> 4000 | 28000 >> 5 = 875 | 128000 >> 5 = 4000 */
+                for (int i = 0; i < 18; i++) begin
+                    pwm_duty[i] <= uart_full_data[i*12 +: 12] << 5;
+                end
             end
         end
     end
+
+    /* PWM REGISTER */
+    logic [19:0] pwm50hz_reg = 0;
     
     always_ff @(posedge clk) begin
         if (!reset_n) begin
@@ -86,15 +83,19 @@ module top (
         end
     end
 
-    assign servo_pwm1 = pwm_duty < pwm50hz_reg;
-    assign debug_a10 = servo_pwm1;
-
+    always_comb begin
+        for (int i = 0; i < 18; i++) begin
+            servo_pwm[i] = (pwm50hz_reg < pwm_duty[i]);
+        end
+    end
+    
     /* LED */
     assign led_done = uart_full_data[8];
     assign led_ready = button_s2;
 
     /* DEBUG */
-    assign debug_l11 = uart1_data_count[0];
-    assign debug_e10 = uart1_data_count[1];
+    assign debug_a10 = uart1_data_count[0];
+    assign debug_l11 = uart1_data_count[1];
+    assign debug_e10 = uart1_data_count[2];
 
 endmodule
